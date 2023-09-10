36e8be3efe8e121246333c3ad29a5df58827e24f
Pedro Luis Gutierrez <83560861+iampedroluis@users.noreply.github.com>
2023-09-10 23:29:43 +0000

    se modifican componentes para un diseño responsive

--- src/front/js/pages/createexcercise.js
+++ src/front/js/pages/createexcercise.js
@@ -13,79 +13,79 @@ export const CreateExcercise = () => {
 
 
     return (
-        <div className="container text-start">
-            <div className="row">
+        <div className="container vh-100 align-items-center">
+            <div className="row mb-4">
                 <div className="col">
                     <Link to="/modules"><i className="fa-solid fa-arrow-left arrow-back"></i></Link>
                 </div>
             </div>
-            <div className="row align-items-center">
-                <div className="col-4">
-                    <div className="img-index">
-                        <img src={logoCM} alt="Logo" className="img-fluid" />
+            <div className="row justify-content-center d-flex justify-content-between ">
+                <div className="col-md-6 text-start">
+                    <div className="embed-responsive embed-responsive-16by9">
+                        <h2 className='bigtext text-line'>
+                            Create <span className='text-color-primary'>Excercise</span>
+                        </h2>
                     </div>
                 </div>
-                <div className="col-8 mt-5 align-items-center">
-                    <div className="row mb-3">
-                        <div className="col-12 text-center mb-4">
-                            <h2 className='bigtext'>Create <span className='text-line'>Exercise</span></h2>
-                        </div>
+                <div className="col-md-6 mt-4 align-items-start">
+                    <div className="h-25 ">
+                        <p>Module:</p>
+                        <div className="btn-group dropend ">
+                        <button className="btn btn-secondary dropdown-toggle " type="button" data-bs-toggle="dropdown" aria-expanded="false" style={{ zIndex: 1 }} >
+                            {selectedModule ? selectedModule : "Select Module"}
+                            
+                        </button>
+                        <ul className="dropdown-menu dropdown-menu-dark">
+                            <li><a className="dropdown-item" href="#" onClick={(e) => handleModuleSelect("HTML", e)}>HTML</a></li>
+                            <li><a className="dropdown-item" href="#" onClick={(e) => handleModuleSelect("CSS", e)}>CSS</a></li>
+                            <li><a className="dropdown-item" href="#" onClick={(e) => handleModuleSelect("JavaScript", e)}>JavaScript</a></li>
+                            <li><a className="dropdown-item" href="#" onClick={(e) => handleModuleSelect("React", e)}>React</a></li>
+                            <li><a className="dropdown-item" href="#" onClick={(e) => handleModuleSelect("Python", e)}>Python</a></li>
+                            <li><a className="dropdown-item" href="#" onClick={(e) => handleModuleSelect("Flask", e)}>Flask</a></li>
+                            <li><a className="dropdown-item" href="#" onClick={(e) => handleModuleSelect("SQL Alchemy", e)}>SQL Alchemy</a></li>
+                            <li><a className="dropdown-item" href="#" onClick={(e) => handleModuleSelect("JWT", e)}>JWT</a></li>
+                        </ul>
                     </div>
-                    <div className="row mb-4 align-items-center">
-                        <div className="col-4 text-end">
-                            <p>Module:</p>
-                        </div>
-                        <div className="col justify-content-start mb-4">
-                            <div className="btn-group dropend">
-                                <button className="btn btn-light dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    {selectedModule ? selectedModule : "Select Module"}
-                                </button>
-                                <ul className="dropdown-menu">
-                                    <li><a className="dropdown-item" href="#" onClick={(e) => handleModuleSelect("HTML", e)}>HTML</a></li>
-                                    <li><a className="dropdown-item" href="#" onClick={(e) => handleModuleSelect("CSS", e)}>CSS</a></li>
-                                    <li><a className="dropdown-item" href="#" onClick={(e) => handleModuleSelect("JavaScript", e)}>JavaScript</a></li>
-                                </ul>
-                            </div>
-                        </div>
                     </div>
-                    <div className="row mb-3 align-items-center">
-                        <div className="col-4 text-end">
-                            <p>Exercise Type</p>
+
+                    <div className="row mt-3">
+                        <div className="">
+                            <p>Excercise Type:</p>
                         </div>
                         <div className="col justify-content-start">
                             <div className="form-check form-check-inline">
                                 <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" />
-                                <label className="form-check-label" htmlFor="inlineRadio1">Fill in the Blank</label>
+                                <label className="form-check-label text-light" htmlFor="inlineRadio1">Fill in the Blank</label>
                             </div>
                             <div className="form-check form-check-inline">
                                 <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2" />
-                                <label className="form-check-label" htmlFor="inlineRadio2">Select Option</label>
+                                <label className="form-check-label text-light" htmlFor="inlineRadio2">Select Option</label>
                             </div>
                             <div className="form-check form-check-inline">
                                 <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3" />
-                                <label className="form-check-label" htmlFor="inlineRadio3">Drag and Drop</label>
+                                <label className="form-check-label text-light" htmlFor="inlineRadio3">Drag and Drop</label>
                             </div>
                         </div>
                     </div>
-                    <div className="row mb-3 align-items-center">
-                        <div className="col-4 text-end">
+                    <div className="row mt-3">
+                        <div className="">
                             <p>Question:</p>
                         </div>
                         <div className="col justify-content-start">
                             <input type="text" className="form-control" id="pregunta" placeholder=""></input>
                         </div>
                     </div>
-                    <div className="row mb-3 align-items-center">
-                        <div className="col-4 text-end">
+                    <div className="row mt-3">
+                        <div className="">
                             <p>Answer:</p>
                         </div>
                         <div className="col justify-content-start">
-                            <input type="text" className="form-control" id="respuesta" placeholder=""></input>
+                            <input type="text" className="form-control" id="answer" placeholder=""></input>
                         </div>
                     </div>
-                    <div className="row">
-                        <div className="col text-center mb-2">
-                            <a href="#" className="btn btn-outline-secondary me-2">Save Changes</a>
+                    <div className="row mt-4">
+                        <div className="col text-center">
+                            <a href="#" className="btn btn-outline-secondary">Create</a>
                         </div>
                     </div>
                 </div>
--- src/front/js/pages/profile.js
+++ src/front/js/pages/profile.js
@@ -10,71 +10,73 @@ export const Profile = () => {
     };
 
     return (
-        <div className="container text-start">
-            <div className="row">
+        <div className="container vh-100 align-items-center">
+            <div className="row mb-4">
                 <div className="col">
-                    <Link to="/modules"><i className="fa-solid fa-arrow-left arrow-back"></i></Link>
+                    <Link to="/profile"><i className="fa-solid fa-arrow-left arrow-back"></i></Link>
                 </div>
             </div>
-            <div className="row align-items-center">
-                <div className="col-4">
-                    <h2 className='bigtext text-line'>
-                        Hi <span className='text-color-primary'>James</span>! This is your profile
-                    </h2>
-                </div>
-                <div className="col-8 mt-5 align-items-center">
-                    <div className="row  d-flex mb-3 align-items-center">
-                        <div className="col-4 text-end">
-                            <p>First Name</p>
-                        </div>
-                        <div className="col justify-content-start">
-                            <input type="text" className="form-control" id="firstName" placeholder="" style={{ maxWidth: "60%" }}></input>
-                        </div>
-                    </div>
-                    <div className="row mb-3 align-items-center">
-                        <div className="col-4 text-end">
-                            <p>Last Name</p>
-                        </div>
-                        <div className="col justify-content-start">
-                            <input type="text" className="form-control" id="lastName" placeholder="" style={{ maxWidth: "60%" }}></input>
-                        </div>
-                    </div>
-                    <div className="row mb-3 align-items-center">
-                        <div className="col-4 text-end">
-                            <p>Email</p>
-                        </div>
-                        <div className="col justify-content-start">
-                            <input type="text" className="form-control" id="email" placeholder=""></input>
-                        </div>
-                    </div>
-                    <div className="row mb-3 align-items-center">
-                        <div className="col-4 text-end">
-                            <p>Teacher</p>
-                        </div>
-                        <div className="col justify-content-start">
-                            <div className="btn-group dropend">
-                                <button className="btn btn-light dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    {selectedTeacher ? selectedTeacher : "Select Your Teacher"}
-                                </button>
-                                <ul className="dropdown-menu">
-                                    <li><a className="dropdown-item" href="#" onClick={(e) => handleTeacherSelect("Arnaldo Perez", e)}>Arnaldo Perez</a></li>
-                                    <li><a className="dropdown-item" href="#" onClick={(e) => handleTeacherSelect("Martin Coimbra", e)}>Martin Coimbra</a></li>
-                                    <li><a className="dropdown-item" href="#" onClick={(e) => handleTeacherSelect("Severus Snape", e)}>Severus Snape</a></li>
-                                </ul>
-                            </div>
-                        </div>
+            <div className="row justify-content-center d-flex justify-content-between">
+                <div className="col-sm-12 col-md-6 text-start">
+                    <div className="embed-responsive embed-responsive-16by9">
+                        <h2 className='bigtext text-line text-break'>
+                            Hi <span className='text-color-primary'>James</span>! This is your profile
+                        </h2>
                     </div>
                 </div>
-                <div className="row">
-                    <div className="col text-center mb-2 ">
-                        <a href="#" className={`btn btn-outline-secondary me-2 ${!selectedTeacher ? 'disabled' : ''}`}>save changes</a>
-                        <Link to={"/changepassword"}><a href="#" className="btn btn-outline-secondary">Change Your Password</a></Link>
+                <div className="col-sm-12 col-md-6 mt-4 align-items-start">
+                    <div className='d-flex justify-content-between mb-2'>
+                        <p className='my-0 me-4'>First Name:</p>
+                        <input type="text" className="form-control" id="firstName" placeholder="" style={{ maxWidth: "60%" }}></input>
                     </div>
+                    <div className='d-flex justify-content-between  mb-2'>
+                        <p className='my-0 me-4'>Last Name:</p>
+                        <input type="text" className="form-control" id="firstName" placeholder="" style={{ maxWidth: "60%" }}></input>
+                    </div>
+                    <div className='d-flex justify-content-between  mb-2'>
+                        <p className='my-0 me-4'>e-mail:</p>
+                        <input type="text" className="form-control" id="firstName" placeholder="" style={{ maxWidth: "60%" }}></input>
+                    </div>
+                    <div className='d-flex justify-content-between  mb-2'>
+                        <p className='my-0 '>Teacher:</p>
+                        <div className="h-25 px-5  ">
+        
+                        <div className="btn-group dropdown-center" >
+                        <button className="btn btn-secondary dropdown-toggle " type="button" data-bs-toggle="dropdown" aria-expanded="false" >
+                            {selectedTeacher ? selectedTeacher : "Select Your Teacher"}
+                            
+                        </button>
+                        <ul className="dropdown-menu dropdown-menu-dark">
+                            <li><a className="dropdown-item" href="#" onClick={(e) => handleTeacherSelect("Arnaldo Perez", e)}>Arnaldo Perez</a></li>
+                            <li><a className="dropdown-item" href="#" onClick={(e) => handleTeacherSelect("Martin Coimbra", e)}>Martin Coimbra</a></li>
+                            <li><a className="dropdown-item" href="#" onClick={(e) => handleTeacherSelect("Nicola Tesla", e)}>Nicola Tesla</a></li>
+                            <li><a className="dropdown-item" href="#" onClick={(e) => handleTeacherSelect("Albert Einstein", e)}>Albert Einstein</a></li>
+                            <li><a className="dropdown-item" href="#" onClick={(e) => handleTeacherSelect("Isaac Newton", e)}>Isaac Newton</a></li>
+                            <li><a className="dropdown-item" href="#" onClick={(e) => handleTeacherSelect("Aristoteles", e)}>Aristoteles</a></li>
+                            <li><a className="dropdown-item" href="#" onClick={(e) => handleTeacherSelect("Galileo Galilei", e)}>Galileo Galilei</a></li>
+                            <li><a className="dropdown-item" href="#" onClick={(e) => handleTeacherSelect("Marie Curie", e)}>Marie Curie</a></li>
+                        </ul>
+                    </div>
+                    </div>
+                    </div>
+                    <div className="row  align-items-center mt-4 justify-content-end">
+                    <div className=" col-sm-12 col-md-6  text-center">
+                        <a href="#" className="btn btn-outline-secondary">Save</a>
+                    </div>
+
+
                 </div>
+
+
+                </div>
+
             </div>
-            <div className="row">
-                <div className="col text-end mb-2">
-                    <p>I’m Professor</p>
+            <div className='row h-25 d-flex justify-content-end align-items-center'>
+                <div className="col-sm-1 col-md-4  text-sm-end justify-content-between">
+                    <div className="d-flex justify-content-between ">
+                        <Link to={"/changepassword"}><a href="#" className="btn btn-outline-secondary">Change password</a></Link>
+                        <a href="#" className="">I'm Professor</a>
+                    </div>
                 </div>
             </div>
         </div>
