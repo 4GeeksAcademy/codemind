bd5f0c33abf4c416ec322dc8ada2871874398430
Pedro Luis Gutierrez <83560861+iampedroluis@users.noreply.github.com>
2023-09-14 23:19:56 +0000

    arreglo create account

--- src/front/js/pages/registro.js
+++ src/front/js/pages/registro.js
@@ -5,30 +5,64 @@ import "../../styles/home.css";
 
 
 export const Registro = () => {
-  const [formData, setFormData] = useState({
+  const { store, actions} = useContext(Context)
+  const initialFormData ={
     firstName: '',
     lastName: '',
     email: '',
     password: '',
     confirmPassword: '',
-  });
+  }
+  const [formData, setFormData] = useState({...initialFormData});
+  const [registrationSuccess, setregistrationSuccess] = useState(null)
+  const [registrationError, setRegistrationError] = useState(null);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    // Aquí puedes agregar la lógica para enviar los datos del formulario a tu servidor o realizar otras acciones necesarias
-    console.log(formData);
+    if (formData.password !== formData.confirmPassword){
+      setRegistrationError("Las contraseñas no coinciden")
+      return;
+    }
+    try{
+      console.log("enviar action con formData con un await")
+      setregistrationSuccess(`Se registró con éxito como ${formData.firstName}`)
+      setRegistrationError(null)
+      setFormData({ ...initialFormData })
+    }catch(error){
+      console.error('Error al Agregar al usuario: ', error)
+
+    }
   };
 
   return (
     <div className="container mt-5">
+
       <div className="row justify-content-center">
+
         <div className="col-md-6">
+        {registrationSuccess && (
+            <div class="alert alert-success d-flex align-items-center" role="alert">
+           
+            <div>
+            <i class="fa-regular fa-circle-check fs-2 fw-bold text-success"></i>  usuario Creado success
+            </div>
+          </div>
+          )}
+          {registrationError && (
+            <div className="alert alert-danger d-flex align-items-center" role="alert">
+            
+            <div>
+            <i class="fa-solid fa-triangle-exclamation fs-2 fw-bold text-danger"></i>  Las contraseñas no coinciden
+            </div>
+          </div>
+          )}
           <h2>Create Account</h2>
+          
           <form onSubmit={handleSubmit}>
             <div className="mb-3">
               <label htmlFor="firstName" className="form-label">
