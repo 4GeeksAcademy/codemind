8766407a455b85e9645d301d221ebbb68c0ce622
Pedro Luis Gutierrez <83560861+iampedroluis@users.noreply.github.com>
2023-09-25 01:56:30 +0000

    funcionalidad signup y login

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -50,15 +50,92 @@ const getState = ({ getStore, getActions, setStore }) => {
 				//reset the global store
 				setStore({ demo: demo });
 			},
-			addUser:  (newUser) => {
-			const {user} = getStore()
-			const updatedUser = { ...user, ...newUser };
-			setStore({ user: updatedUser });
-        	console.log("USER DESDE EL FLUX", updatedUser);
-			localStorage.setItem('user', JSON.stringify(updatedUser))
+			addUser:  async (newUser) => {
+			// const {user} = getStore()
+			// const updatedUser = { ...user, ...newUser };
+			// setStore({ user: updatedUser });
+        	// console.log("USER DESDE EL FLUX", updatedUser);
+			// localStorage.setItem('user', JSON.stringify(updatedUser))
+			const url = process.env.BACKEND_URL + '/api/user'
+			const options = {
+				method:  'POST',
+				body: JSON.stringify(newUser),
+				headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'}
 			}
-		}
-	};
-};
+			try {
+				const resp = await fetch(url, options)
+				if(resp.ok){
+					console.log('La solicitud se realizó con éxito');
+				}else {
+					console.error('La solicitud no se realizó con éxito');
+			 }
+			} catch (error) {
+				console.error(error)
+			}
+			},
+		
+			loginUser: async (userCredentials) => {
+                let { user } = getStore();
+                console.log("loginUser called");
+                const url = process.env.BACKEND_URL + '/api/login';
+                const options = {
+                    method: 'POST',
+                    body: JSON.stringify(userCredentials),
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Access-Control-Allow-Origin': '*'
+                    }
+                };
+                try {
+                    const resp = await fetch(url, options);
+                    if (resp.ok) {
+                        const data = await resp.json();
+                        console.log('La solicitud se realizó con éxito');
 
+                        // Guardar el token en localStorage
+                        localStorage.setItem('userToken', data.token);
+
+                        // Actualizar store.user con los datos del usuario
+                        setStore({ user: { ...user, ...data } });
+
+                        // Obtener los datos completos del usuario
+                        getActions().getUser();
+                    } else {
+                        console.log('La solicitud no se realizó con éxito');
+                    }
+                } catch (error) {
+                    console.error(error);
+                }
+            },
+            getUser: async () => {
+                let { user } = getStore();
+                console.log("GET USER called");
+                const url = process.env.BACKEND_URL + '/api/' + user.userid;
+                const options = {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Access-Control-Allow-Origin': '*',
+                        'Authorization': `Bearer ${localStorage.getItem('userToken')}`
+                    }
+                };
+                try {
+                    const resp = await fetch(url, options);
+                    if (resp.ok) {
+                        const data = await resp.json();
+                        console.log('La solicitud se realizó con éxito');
+
+                        // Actualizar store.user con los nuevos datos del usuario
+                        setStore({ user: { ...user, ...data } });
+                        console.log("user getUSER", user);
+                    } else {
+                        console.log('La solicitud no se realizó con éxito');
+                    }
+                } catch (error) {
+                    console.error(error);
+                }
+            }
+        }
+    };
+};
 export default getState;
